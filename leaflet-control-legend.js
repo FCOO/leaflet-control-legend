73e86f56ff6425dbb9879f70329699d42c065130
Jesper Baasch-Larsen <jla@fcoo.dk>
2015-09-30 21:40:39 +0200

    minimizable + timezone aware

--- leaflet-control-legend.js
+++ leaflet-control-legend.js
@@ -11,20 +11,37 @@
     L.Control.Legend = L.Control.extend({
         options: {
             position: "bottomleft",
-            language: "en"
+            language: "en",
+            timezone: 'local',
+            collapsedInfo: null
         },
             
         legendOptions: {},
 
-        initialize: function(map, options) {
+        initialize: function(options) {
             L.Util.setOptions(this, options);
-            this._map = map;
+            this._map = null;
+            if (this.options.collapsedInfo === null) {
+                this.options.collapsedInfo = true;
+                if (typeof window.matchMedia != "undefined" || typeof window.msMatchMedia != "undefined") {
+                    var mq = window.matchMedia('screen and (min-width: 641px) and (min-height: 641px)');
+                    if (mq.matches) {
+                        this.options.collapsedInfo = false;
+                    }
+                }
+            }
             this._container = L.DomUtil.create('div', 'fcoo-legend-container');
             this._container.style.display = 'none';
             this._legendCounter = 0;
         },
 
         onAdd: function(map) {
+            var that = this;
+            // Whenever the timezone is changed we will change the internal timezone
+            map.on("timezonechange", function(data) {
+                that.options.timezone = data.timezone;
+                that._redrawLegend();
+            });
             return this._container;
         },
 
@@ -54,53 +71,93 @@
         },
 
         _redrawLegend: function() {
+            var that = this;
             this._container.innerHTML = ''; // clear container
+            //var containerInner = L.DomUtil.create('div', 'fcoo-legend-container', this._container);
+            //var btn_min = L.DomUtil.create('div', 'fcoo-legend-container-btn-minimize', this._container);
+            //btn_min.innerHTML = '-';
+            //$(this._container).append('<div>New content</div>');
+            //('div', 'fcoo-legend-container-btn-minimize');
+            //.fcoo-legend-item-btn-minimize
             var isLeft = this.options.position.indexOf('left') !== -1;
             var cssFloat = isLeft ? 'left' : 'right';
             var lang = this.options.language;
-            for (var idx in this.legendOptions) {
-                var imgUrl = this.legendOptions[idx].imageUrl;
-                var attribution = this.legendOptions[idx].attribution;
-                var lastUpdated = this.legendOptions[idx].lastUpdated;
-                var longName = this.legendOptions[idx].longName;
-                var units = this.legendOptions[idx].units;
-                units = this._(units, lang);
-                var item = L.DomUtil.create('div', 'fcoo-legend-item', this._container);
+            $.each(this.legendOptions, function(idx, options) {
+                var imgUrl = options.imageUrl;
+                var attribution = options.attribution;
+                var lastUpdated = options.lastUpdated;
+                var epoch = options.epoch;
+                var longName = options.longName;
+                var units = options.units;
+                units = that._(units, lang);
+                var item = L.DomUtil.create('div', 'fcoo-legend-item', that._container);
                 item.style.cssFloat = cssFloat;
-                if (isLeft) {
-                    item.style.marginRight = '10px';
-                } else {
-                    item.style.marginLeft = '10px';
-                }
-                var item_img = L.DomUtil.create('img', 'fcoo-legend-item-image', item);
+                var itemDiv = L.DomUtil.create('div', 'fcoo-legend-item-div', item);
+                var item_img = L.DomUtil.create('img', 'fcoo-legend-item-image', itemDiv);
                 item_img.src = imgUrl;
 
                 var leginner = '';
                 var item_text = L.DomUtil.create('div', 'fcoo-legend-item-text', item);
+
+                var lnameP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text);
+                var btn = L.DomUtil.create('i', 'fa-caret-square-o-down fa', lnameP);
+                btn.innerHTML = '&nbsp;&nbsp;';
                 if (longName !== undefined) {
                     var longNameCap =
                         longName.charAt(0).toUpperCase() +
                         longName.slice(1);
-                    longNameCap = this._(longNameCap, lang);
-                    var lnameP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text);
+                    longNameCap = that._(longNameCap, lang);
+                    //var lnameP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text);
                     if (units !== undefined && units !== '') {
                         longNameCap = longNameCap + ' [' + units + ']';
                     }
-                    lnameP.innerHTML = longNameCap;
+                    var lspan = L.DomUtil.create('span', '', lnameP);
+                    lspan.innerHTML = longNameCap;
                 }
+                //var btnDiv = L.DomUtil.create('div', 'fcoo-legend-item-hide', btn);
+
+                var item_text_extra = L.DomUtil.create('div', 'fcoo-legend-item-text', item);
                 if (attribution !== undefined) {
-                    var source = this._('Source', lang) + ': ' + attribution;
-                    var attrP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text);
+                    var source = that._('Source', lang) + ': ' + attribution;
+                    var attrP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text_extra);
                     attrP.innerHTML = source;
                 }
+
+                // Date formatter
+                var dateAsHTML = function( m, language, tz ){
+                    var dateFormat = 'DD-MMM-YY HH:mm',
+                        localTxt = language == 'da' ? 'lokal' : 'local',
+                        result;
+                    if (tz == 'local') {
+                        result = m.local().format(dateFormat)+ '&nbsp;('+localTxt+')';
+                    } else {
+                        result = m.utc().format(dateFormat) + '&nbsp;(UTC)';
+                    }
+                    return result;
+                };
+
                 if (lastUpdated !== undefined) {
-                    var luString = this._('Updated', lang) + ': ' +
-                        lastUpdated.utc().format('YYYY-MM-DDTHH:mm') + ' UTC';
-                    var lastP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text);
+                    var luString = that._('Updated', lang) + ': ' +
+                        dateAsHTML(lastUpdated, lang, that.options.timezone);
+                    var lastP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text_extra);
                     lastP.innerHTML = luString;
                 }
-                var br = L.DomUtil.create('br', '', this._container);
-            }
+                if (epoch !== undefined) {
+                    var eString = that._('Analysis', lang) + ': ' +
+                        epoch.utc().format('YYYY-MM-DDTHH:mm') + ' UTC';
+                    var epochP = L.DomUtil.create('p', 'fcoo-legend-item-text-p', item_text_extra);
+                    epochP.innerHTML = eString;
+                }
+                var br = L.DomUtil.create('br', '', that._container);
+
+                $(btn).click(function(){
+                    $(this).toggleClass('fa-caret-square-o-right');
+                    $(item_text_extra).slideToggle();
+                });
+                if (that.options.collapsedInfo) {
+                    $(btn).click();
+                }
+            });
         },
 
         /**
@@ -119,6 +176,7 @@
                 da: {
                       'Source': 'Kilde',
                       'Updated': 'Opdateret',
+                      'Analysis': 'Analyse',
                       'Wave height': 'Bølgehøjde',
                       'Mean wave period': 'Bølgeperiode',
                       'Vel.': 'Strømstyrke',
